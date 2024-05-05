431f08bbfd3d6480964c19d8ed79fdcbad35ca77
XimenaFerrer28 <150406879+XimenaFerrer28@users.noreply.github.com>
2024-05-05 01:01:40 -0600

    Modificación de Email

--- canciones/src/main/java/humbe/canciones/controller/clienteController.java
+++ canciones/src/main/java/humbe/canciones/controller/clienteController.java
@@ -1,5 +1,4 @@
 package humbe.canciones.controller;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +12,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-
 import humbe.canciones.model.entity.cliente;
 import humbe.canciones.server.ICliente;
 import humbe.canciones.server.impl.EmailService;
-
 @RestController
 @RequestMapping("/api/v1")
 public class clienteController {
-
 	@Autowired
 	private ICliente clienteservice;
-	
 	@Autowired
 	private EmailService emailservi;
-	private static final Logger logger = LoggerFactory.getLogger(clienteController.class);
-	
+	private static final Logger logger =
+	LoggerFactory.getLogger(clienteController.class);
 	@PostMapping("cliente")
 	@ResponseStatus(HttpStatus.CREATED)
 	public cliente create(@RequestBody cliente cliente) {
@@ -38,45 +32,46 @@ public class clienteController {
 		emailservi.enviarCorreoCacnionGuardado(saveCliente.getEmail());
 		return saveCliente;
 	}
-	
 	@PutMapping("cliente/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
-	public cliente update(@PathVariable Integer id,  @RequestBody cliente cliente) {
+	public cliente update(@PathVariable Integer id, @RequestBody cliente
+	cliente) {
 		cliente existingClien = clienteservice.findById(id);
 		if(existingClien == null) {
-			logger.warn("Cancion no encontrada ");
-		}
-		cliente.setID_cancion(id);
-		cliente updatedCleinte = clienteservice.save(cliente);
-		emailservi.enviarCorreoCancionModificado(updatedCleinte.getEmail());
-		return updatedCleinte;
+		logger.warn("Cancion no encontrada ");
 	}
-	
+	cliente.setID_cancion(id);
+	cliente updatedCleinte = clienteservice.save(cliente);
+	emailservi.enviarCorreoCancionModificado(updatedCleinte.getEmail());
+	return updatedCleinte;
+	}
 	@DeleteMapping("cliente/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Integer id) {
 		cliente clienteDelete = clienteservice.findById(id);
-	    if (clienteDelete != null) {
-	        clienteservice.delete(clienteDelete);
-	        
-	        // Envía el correo de notificación al eliminar la canción
-	        String destinatario = clienteDelete.getEmail();
-	        String cancionEliminada = clienteDelete.getNombre(); // O cualquier otro campo que identifique la canción
-	        emailservi.enviarCorreoCacnionEliminado(destinatario, cancionEliminada); 
-	    } else {
-	        // Si el producto no existe, puedes manejar la situación de alguna manera, como lanzar una excepción o simplemente registrar un mensaje de error.
-	        logger.warn("Intento de eliminar un producto con ID no válido: " + id);
-	        // Aquí puedes lanzar una excepción, registrar un mensaje de error, etc.
-	    }
+		if (clienteDelete != null) {
+			clienteservice.delete(clienteDelete);
+			// Envía el correo de notificación al eliminar la canción
+			String destinatario = clienteDelete.getEmail();
+			String cancionEliminada = clienteDelete.getNombre(); // O
+			cualquier otro campo que identifique la canción
+			emailservi.enviarCorreoCacnionEliminado(destinatario,
+			cancionEliminada);
+		} else {
+			// Si el producto no existe, puedes manejar la situación de
+			alguna manera, como lanzar una excepción o simplemente registrar un mensaje
+			de error.
+			logger.warn("Intento de eliminar un producto con ID no válido: "
+			+ id);
+			// Aquí puedes lanzar una excepción, registrar un mensaje de
+			error, etc.
+		}
 	}
-	
 	@GetMapping("cliente/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public cliente showById(@PathVariable Integer id) {
 		return clienteservice.findById(id);
 	}
-	
-	
 	@GetMapping("cliente")
 	@ResponseStatus(HttpStatus.OK)
 	public Iterable<cliente> show() {
