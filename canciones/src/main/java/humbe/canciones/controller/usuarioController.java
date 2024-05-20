6173e26108ef6e92a0db0f50a88bdca834795a2d
XimenaFerrer28 <150406879+XimenaFerrer28@users.noreply.github.com>
2024-05-19 20:26:16 -0600

    Update usuarioController.java

--- canciones/src/main/java/humbe/canciones/controller/usuarioController.java
+++ canciones/src/main/java/humbe/canciones/controller/usuarioController.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,15 +25,14 @@ public class usuarioController {
 	private IUsuario usuarioService;
 	
 	@Autowired
-	private EmailService emailServi;
+	private EmailService emailServi; 
 	private static final Logger logger = LoggerFactory.getLogger(usuarioController.class);
 
 	@PostMapping("usuario")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Usuario create(@RequestBody Usuario usuario) {
 		Usuario saveUsuario = usuarioService.save(usuario);
-		String usuario23 = saveUsuario.getNombre();
-		emailServi.enviarCorreoAutenticacion(saveUsuario.getEmail(), usuario23);
+		emailServi.enviarCorreoAutenticacion(saveUsuario.getEmail(), saveUsuario.getToken());
 		return saveUsuario;
 	}
 	
@@ -49,4 +50,27 @@ public class usuarioController {
 			logger.warn("Intento de eliminar un producto con ID no válido: " + id);
 		}
 	}
+	
+	@PostMapping("usuario/login")
+    public ResponseEntity<Usuario> login(@RequestBody Usuario loginRequest) {
+        Usuario usuario = usuarioService.finByEmailPassword(loginRequest.getEmail(), loginRequest.getPassword());
+        if (usuario != null && usuario.getConfirmado() != null && usuario.getConfirmado() == 1) {
+            return ResponseEntity.ok(usuario);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+            
+        }
+    }
+	
+	 @GetMapping("usuario/verificar/{token}")
+	    public ResponseEntity<String> verificar(@PathVariable String token) {
+	        Usuario usuario = usuarioService.findByToken(token);
+	        if (usuario != null) {
+	            usuario.setConfirmado(1);
+	            usuarioService.save(usuario);
+	            return ResponseEntity.ok("La cuenta ha sido verificada exitosamente");
+	        } else {
+	            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo verificar la cuenta");
+	        }
+	    }
 }
